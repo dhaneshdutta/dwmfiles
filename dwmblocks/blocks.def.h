0d9286f5411bfd7b8e3364829c3159d1e5a3df53
dhaneshdutta <dhanesh1551@gmail.com>
2024-09-29 02:28:56 +0530

    bar changes

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -5,10 +5,12 @@ static const Block blocks[] = {
     {"🔋 ",     "acpi -b | awk '{print $4}' | tr -d ','",                       1,     12},
     {"🔊 ",     "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }'",       1,     13},
     {"💡 ",     "brightnessctl -m | cut -d',' -f4 | tr -d '%'",                 1,     14},
-    {"📅 ",     "date '+%d %b %a'",                                             1,     15},
-    {"🕒 ",     "date '+%I:%M %p'",                                             1,     16},
+    {"📶 ",     "nmcli -t -f active,ssid dev wifi | awk -F: '$1==\"yes\"{print $2}' || echo 'Disconnected'", 5, 15},
+    {"🎧 ",    "bluetoothctl info | sed -n '/Name/{s/.*: //;p;q}' || echo 'Disconnected'", 5, 16},
+    {"📅 ",     "date '+%d %b %a'",                                             60,    17},
+    {"🕒 ",     "date '+%I:%M %p'",                                             60,    18},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " ";
+static char delim[] = "  ";
 static unsigned int delimLen = 2;
